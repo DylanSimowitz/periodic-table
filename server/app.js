d896c06950331fa9df8c1ad1295a0c6b4193e0f4
Dylan Simowitz <dsimowitz@gmail.com>
2016-12-04 16:20:03 -0800

    use port 5000

--- server/app.js
+++ server/app.js
@@ -22,6 +22,6 @@ router.route('/data')
     res.sendFile(path.join(__dirname, '/static/database.json'))
   })
 app.use('/api', router);
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Example app listening on port 3000');
+app.listen(process.env.PORT || 5000, function() {
+    console.log('Example app listening on port 5000');
 });
